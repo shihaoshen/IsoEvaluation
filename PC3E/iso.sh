4aea8167e87a90df6bb56209fc3dc25ab572c159
shihaoshen <shihaoshen@gmail.com>
2019-02-26 09:32:17 -0600

    Add files via upload

--- PC3E/iso.sh
+++ PC3E/iso.sh
@@ -19,4 +19,4 @@ od=/mnt/isilon/xing_lab/shens/ISO_ReRun/PC3E/iso_output/
 
 thisdate="$(date +"%M-%H-%m-%d-%Y")"
 python $bin/rMATS-ISO.py module --gtf $gtf --bam $list -o $od --novel-sj --exon-thres 30 --iso-cnt-thres 5000 >$log/iso_log_$thisdate.txt 2>$log/iso_log_error_$thisdate.txt
-python $bin/rMATS-ISO.py stat --psi-low 0.05 --bam $list -o $od >$log/iso_stat_log_$thisdate.txt 2>$log/iso_stat_log_error_$thisdate.txt
+python $bin/rMATS-ISO.py stat --psi-low 0.10 --bam $list -o $od >$log/iso_stat_log_$thisdate.txt 2>$log/iso_stat_log_error_$thisdate.txt
